443fc480653562e78caac02200941b994a1db95d
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-10-05 16:08:26 +0200

    Created a RESTful API with the use of CRUD endpoints

---
+++ server.js
@@ -0,0 +1,284 @@
+const express = require("express"),
+  morgan = require("morgan"),
+  fs = require("fs"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid"),
+  app = express();
+
+app.use(bodyParser.json());
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+let users = [
+  {
+    id: 1,
+    name: "Shayan",
+    favouriteMovies: [],
+    email: "Shayan2018@gmail.com",
+    password: "sjdjnd",
+    Birth: "18-02-2018",
+  },
+  {
+    id: 2,
+    name: "Shahir",
+    favouriteMovies: ["The Pianist"],
+    email: "Shahir2020@gmail.com",
+    password: "idcjooo",
+    Birth: "06-01-2020",
+  },
+];
+
+let topMovies = [
+  {
+    Title: "Saving Private Rayan",
+    Description:
+      "Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
+
+    Generes: {
+      Name: "War",
+      Description:
+        "War film is a film genre concerned with warfare, typically about naval, air, or land battles, with combat scenes central to the drama",
+    },
+    Year: "1998",
+    Director: {
+      name: "Steven Spielberg",
+      Bio: "One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood's best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Jaws (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.",
+      Birth: 1946,
+    },
+    ImageURL:
+      "https://www.imdb.com/title/tt0120815/mediaviewer/rm1924732160/?ref_=tt_ov_i",
+    Featured: false,
+    Stars: "Tom Hanks, Matt Damon, Tom Sizemore",
+  },
+  {
+    Title: "The Shawshank Redemption",
+    Description:
+      "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+    Generes: {
+      Name: "Drama",
+      Description:
+        "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.",
+    },
+    Year: "1994",
+    Director: {
+      name: "Frank Darabont",
+      Bio: "Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution. Brought to America as an infant, he settled with his family in Los Angeles and attended Hollywood High School. His first job in movies was as a production assistant on the 1981 low-budget film",
+      Birth: 1959,
+    },
+    ImageURL:
+      "https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i",
+    Stars: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    Featured: false,
+  },
+  {
+    Title: "Forrest Gump",
+    Description:
+      "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart.",
+    Generes: {
+      Name: "Romance",
+      Description:
+        "Romance films involve romantic love stories recorded in visual media for broadcast in theatres or on television that focus on passion, emotion, and the affectionate romantic involvement of the main characters. Typically their journey through dating. ",
+    },
+    Year: "1994",
+    Director: {
+      name: "Robert Zemeckis",
+      Bio: "A whiz-kid with special effects, Robert is from the Spielberg camp of film-making (Steven Spielberg produced many of his films). Usually working with writing partner Bob Gale, Robert's earlier films show he has a talent for zany comedy (Romancing the Stone (1984), 1941 (1979)) and special effect vehicles (Who Framed Roger Rabbit (1988) and Back to the Future (1985)). His later films have become more serious, with the hugely successful Tom Hanks vehicle Forrest Gump (1994) and the Jodie Foster film Contact (1997), both critically acclaimed movies. Again, these films incorporate stunning effects. Robert has proved he can work a serious story around great effects.",
+      Birth: 1952,
+    },
+    ImageURL:
+      "https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672/?ref_=tt_ov_i",
+    Stars: "Tom Hanks, Robin Wright, Gary Sinise",
+    Featured: false,
+  },
+  // {
+  //   Title: "The Dark Knight",
+  //   Generes: ["Action", "Thriller", "Mystery", "Comic"],
+  //   Year: "1994",
+  //   Director: "Christopher Nolan",
+  //   Stars: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+  // },
+  // {
+  //   Title: "Schindler's List",
+  //   Generes: ["Biography", "Drama", "History", "War"],
+  //   Year: "1993",
+  //   Director: "Steven Spielberg",
+  //   Stars: ["Liam Neeson", "Ralph Fiennes", "Ben Kingsley"],
+  // },
+  // {
+  //   Title: "The Green Mile",
+  //   Generes: ["Crime", "Drama", "Fantasy"],
+  //   Year: "1999",
+  //   Director: "Frank Darabont",
+  //   Stars: ["Tom Hanks", "Michael Clarke Duncan", "David Morse"],
+  // },
+  // {
+  //   Title: "The Pianist",
+  //   Generes: ["Biography", "Drama", "Music"],
+  //   Year: "2002",
+  //   Director: "Roman Polanski",
+  //   Stars: ["Adrien Brody", "Thomas Kretschmann", "Frank Finlay"],
+  // },
+  // {
+  //   title: "Gladiator",
+  //   Generes: ["Action", "Adventure", "Drama"],
+  //   Year: "2000",
+  //   Director: "Ridley Scott",
+  //   Stars: ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+  // },
+  // {
+  //   title: "A Separation",
+  //   Generes: ["Drama", "Suspence"],
+  //   Year: "2011",
+  //   Director: "Asghar Farhadi",
+  //   Stars: ["Payman Maadi", "Leila Hatami", "Sareh Bayat"],
+  // },
+  // {
+  //   title: "Fight Club",
+  //   Generes: ["Drama", "Action"],
+  //   Year: "1999",
+  //   Director: "David Fincher",
+  //   Stars: ["Brad Pitt", "Edward Norton", "Meat loaf"],
+  // },
+];
+
+app.get("/", (req, res) => {
+  let responseText = "Welcome to the movie world!";
+  res.send(responseText);
+});
+
+// Allow new users to register [CREATE]
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("user need names");
+  }
+});
+
+// Allow users to update their user info (username) [UPDATE]
+app.put("/users/:id", (req, res) => {
+  // Destructuring
+  const { id } = req.params;
+  const updatedUser = req.body;
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    user.email = updatedUser.email;
+    user.password = updatedUser.password;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// Allow users to add a movie to their list of favorites (showing only a text that a movie has been added) [CREATE]
+app.post("/users/:id/:movieTitle", (req, res) => {
+  // Destructuring
+  const { id, movieTitle } = req.params;
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favouriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}\'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed) [DELETE]
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  // Destructuring
+  const { id, movieTitle } = req.params;
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favouriteMovies = user.favouriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from the user ${id}\'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// Allow existing users to deregister (showing only a text that a user email has been removed) [DELETE]
+app.delete("/users/:id", (req, res) => {
+  // Destructuring
+  const { id } = req.params;
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user = users.filter((user) => user.id != id);
+    // res.json(user);
+    res.status(200).send(`user ${id} has been removed from the list`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// Return all movies to the user [Read]
+
+app.get("/topMovies", (req, res) => {
+  res.status(200).json(topMovies);
+});
+
+// Return data about a single movie by title to the user [Read]
+app.get("/topMovies/:title", (req, res) => {
+  const title = req.params.title;
+  const movie = topMovies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
+});
+
+// Return data about a genre (description) by name/title (e.g., “Thriller”) [Read]
+app.get("/topMovies/genere/:genereName", (req, res) => {
+  const { genereName } = req.params;
+  const genere = topMovies.find(
+    (movie) => movie.Generes.Name === genereName
+  ).Generes;
+
+  if (genere) {
+    res.status(200).json(genere);
+  } else {
+    res.status(400).send("no such genere");
+  }
+});
+
+// Return data about a director (bio, birth year, death year) by name [Read]
+app.get("/topMovies/director/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = topMovies.find(
+    (director) => director.Director.name === directorName
+  ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director");
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(8080, () => {
+  console.log("My app is listening on port 8080");
+});
