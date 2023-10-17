d78393f6b7ee772eb61e60d50b4c60d29859bc41
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-10-17 07:03:55 +0200

    Included authentication for API access to the user
    JWT Authentication and HTML authentication

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let auth = require("./auth")(app);
+const passport = require("passport");
+require("./passport");
+
 // connecting to the dtabase
 mongoose.connect("mongodb://127.0.0.1:27017/MyFlixDBMONGO", {
   useNewUrlParser: true,
@@ -49,16 +53,20 @@ app.get("/users", async (req, res) => {
 
 //
 // Return all movies to the user [Read]
-app.get("/movies", async (req, res) => {
-  await Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error:" + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    await Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error:" + err);
+      });
+  }
+);
 
 // //
 // Return data about a single movie by title to the user [Read]
@@ -140,27 +148,34 @@ app.post("/users", async (req, res) => {
 
 //
 // update user info by id
-app.put("/users/:_id", async (req, res) => {
-  await Users.findOneAndUpdate(
-    { UserId: req.params.UserId },
-    {
-      $set: {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday,
+app.put(
+  "/users/:_id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    if (req.user.UserID !== req.params.UserID) {
+      return res.status(400).send("Permission denied");
+    }
+    await Users.findOneAndUpdate(
+      { UserID: req.params.UserID },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        },
       },
-    },
-    { new: true }
-  )
-    .then((updateUser) => {
-      res.json(updateUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error:" + err);
-    });
-});
+      { new: true }
+    )
+      .then((updateUser) => {
+        res.json(updateUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error:" + err);
+      });
+  }
+);
 
 //
 // Add a  movie to a user's list of favourites
