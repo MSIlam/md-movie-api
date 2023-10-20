ead5c19537078eeafaf656c5f96721a21ef1e77d
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-10-20 07:43:29 +0200

    Corrected typo

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ let movieSchema = mongoose.Schema({
 
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
-  Password: { type: String, requied: true },
+  Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birth_date: Date,
   FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
