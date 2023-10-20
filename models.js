56ef1f669da7e3c775540ae7031a444b3e7e1148
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-10-20 16:22:03 +0200

    changed filetype

--- models.js
+++ models.js
@@ -12,7 +12,7 @@ let movieSchema = mongoose.Schema({
   },
   ImageURL: String,
   Stars: String,
-  Featured: String,
+  Featured: Boolean,
   Genres: {
     Name: String,
     Description: String,
