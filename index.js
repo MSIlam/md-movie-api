c73e744b21f802ed91821cb7d1568e45a83886c0
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-10-21 11:11:25 +0200

    Changed the cors for all domains

--- index.js
+++ index.js
@@ -25,26 +25,27 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS implementation
 const cors = require("cors");
-let allowedOrigins = [
-  "http://localhost:8080",
-  "https://myflix-mi-e89972ef7472.herokuapp.com",
-];
+app.use(cors());
+// let allowedOrigins = [
+//   "http://localhost:8080",
+//   "https://myflix-mi-e89972ef7472.herokuapp.com",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth.js")(app);
 const passport = require("passport");
